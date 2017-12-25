949be94b07c1f609c85bf717a0fa29cb5aa857c4
Evilcrow <Evilcrow486@gmail.com>
2017-12-25 21:12:33 +0800

    Update memory managemant about recovery!

---
+++ v1.0/memory_recovery.h
@@ -0,0 +1,16 @@
+#ifndef _MEMORY_RECOVERY_H
+#define _MEMORY_RECOVERY_H
+#include "list_node.h"
+#include "block_node.h"
+
+void memory_recovery(list **process_list, Node **block);
+
+int memory_recovery_judge(list_node *list_node, block_node *block_node, int process_ID);
+
+list_node *get_list_node(list *process_list, int process_ID);
+
+block_node *get_block_node(Node *block, list_node *list_node);
+
+int get_process_ID(void);
+
+#endif
